1479e3920bb8c2a5337f0766c509b0a0fe662e76
Andres Betancourt <aebb001@gmail.com>
2020-10-24 14:46:40 -0400

    Updates

---
+++ precache-manifest.faa9fe4a88ef6427087ae8c8ea33175b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "390953adb52c2569867f2b704c13bbbd",
+    "url": "/piedra-papel-tijeras/index.html"
+  },
+  {
+    "revision": "a9de0c4e9b017a7f4ab1",
+    "url": "/piedra-papel-tijeras/static/css/main.5379aedb.chunk.css"
+  },
+  {
+    "revision": "2f3dc181172df46c2e05",
+    "url": "/piedra-papel-tijeras/static/js/2.d211568a.chunk.js"
+  },
+  {
+    "revision": "f5fc2afae58c4ffc30c6148864765d4b",
+    "url": "/piedra-papel-tijeras/static/js/2.d211568a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a9de0c4e9b017a7f4ab1",
+    "url": "/piedra-papel-tijeras/static/js/main.119df95c.chunk.js"
+  },
+  {
+    "revision": "d62a4121810a30510e25",
+    "url": "/piedra-papel-tijeras/static/js/runtime-main.ba470535.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/piedra-papel-tijeras/precache-manifest.faa9fe4a88ef6427087ae8c8ea33175b.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/piedra-papel-tijeras/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
